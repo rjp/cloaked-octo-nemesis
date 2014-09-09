99180aa8b38e17ffd202e8b8c02e1928b47aa276
rjp <rjpartington@gmail.com>
2014-09-10 00:48:15 +0100

    Changes in function signatures Generate a random set of dots with uniform randicity

--- bridson.c
+++ bridson.c
@@ -5,7 +5,23 @@
 /* We need to keep track of active vs inactive points */
 typedef struct { int x; int y; } point;
 
-void
-generate_samples(int **output, int width, int height, int annulus)
+int
+random_under(int max)
 {
+    long r_val = random();
+    return (int)(r_val % max);
+}
+
+void
+generate_samples(int width, int height, int output[height][width], int annulus)
+{
+    /* Currently we fake this by generating a dreadful uniform random
+     * sample of points based on `width` * `height`.
+     */
+    int dots = (width * height) / 20, i;
+
+    for(i=0; i<dots; i++) {
+        int r_x = random_under(width), r_y = random_under(height);
+        output[r_y][r_x] = 255;
+    }
 }
---
+++ bridson.h
@@ -0,0 +1,5 @@
+#ifndef __BRIDSON_H
+#define __BRIDSON_H 1
+
+void generate_samples(int width, int height, int output[height][width], int annulus);
+#endif
