74e2c615197bebd76120d246b032faded8753123
rjp <rjpartington@gmail.com>
2014-09-10 00:34:48 +0100

    Beginning the Bridson

---
+++ bridson.c
@@ -0,0 +1,11 @@
+/* Generate a Poisson Disc using Bridson */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* We need to keep track of active vs inactive points */
+typedef struct { int x; int y; } point;
+
+void
+generate_samples(int **output, int width, int height, int annulus)
+{
+}
