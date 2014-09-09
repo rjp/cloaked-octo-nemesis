cb0554c98c728cf5e566ea1908474d303c0bbfa3
rjp <rjpartington@gmail.com>
2014-09-10 00:28:03 +0100

    Basic PGM support that will be used for visualising the Bridson

---
+++ my-ppm.c
@@ -0,0 +1,16 @@
+#include "my-ppm.h"
+
+void
+output_pgm(FILE *stream, int **pgm, int width, int height)
+{
+    int i, j;
+
+    fprintf(stream, "P2 %d %d 255\n", width, height);
+    for(i=0; i<height; i++) {
+        for(j=0; j<width; j++) {
+            fprintf(stream, "%d ", pgm[i][j]);
+            if (j % 15 == 14) { fprintf(stream, "\n"); }
+        }
+        fprintf(stream, "\n");
+    }
+}
---
+++ my-ppm.h
@@ -0,0 +1,8 @@
+#ifndef __MY_PPM_H
+#define __MY_PPM_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void output_pgm(FILE *stream, int **pgm, int width, int height);
+#endif
