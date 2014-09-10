da348099a99a4bbad6d56123ec18e86a9acc9e1b
rjp <rjpartington@gmail.com>
2014-09-10 01:26:24 +0100

    Randomise the randoms

--- main.c
+++ main.c
@@ -14,7 +14,9 @@ main(void)
         for(j=0; j<512; j++) {
             landscape[i][j] = 0;
         }
-    }        
+    }
+    /* Start with a completely random state for the minute */
+    srandomdev();
     generate_samples(512, 512, landscape, 20);
     output_pgm(stderr, 512, 512, landscape);
 }
