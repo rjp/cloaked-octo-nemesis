d4befdb7b0879b1b8e0b657da28deca123553937
rjp <rjpartington@gmail.com>
2014-09-11 22:47:54 +0100

    Add ocean colouring for "deep" water Return slight variants on each colour for a bit of variety in the Voronoi cells

--- colour.c
+++ colour.c
@@ -1,31 +1,68 @@
 #include "landscape.h"
 #include "colour.h"
 
+rgb c_ocean = { 0, 0, 128 };
 rgb c_sand = { 255, 192, 64 };
 rgb c_water = { 0, 0, 255 };
 rgb c_grass = { 0, 255, 0 };
 rgb c_stone = { 128, 128, 128 };
 rgb c_snow = { 255, 255, 255 };
 
+int
+clamp_byte(int v)
+{
+    if (v > 255) { return 255; }
+    if (v < 0)   { return 0; }
+    return v;
+}
+
+rgb
+twiddle(rgb in, int grey, int scale)
+{
+    rgb out = in;
+    int dr, dg, db;
+
+    dr = (int)(scale * m1_p1());
+    if (grey) {
+        dg = dr; db = dr;
+    }
+    else {
+        dg = (int)(scale * m1_p1());
+        db = (int)(scale * m1_p1());
+    }
+
+    out = (rgb){
+        clamp_byte(in.r+dr),
+        clamp_byte(in.g+dg),
+        clamp_byte(in.b+db)
+    };
+
+    return out;
+}
+
 rgb
 colour_by_height(double scaled_height)
 {
+    if (scaled_height < 0.10) {
+        return twiddle(c_ocean, 0, 8);
+    }
+
     if (scaled_height < 0.20) {
-        return c_water;
+        return twiddle(c_water, 0, 16);
     }
 
     if (scaled_height < 0.25) {
-        return c_sand;
+        return twiddle(c_sand, 0, 8);
     }
 
     if (scaled_height > 0.80) {
-        return c_snow;
+        return twiddle(c_snow, 1, 32);
     }
 
     if (scaled_height > 0.70) {
-        return c_stone;
+        return twiddle(c_stone, 1, 32);
     }
 
-    return c_grass;
+    return twiddle(c_grass, 0, 24);
 }
 
